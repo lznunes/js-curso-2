c8615caade74d666074f417ae9316b0ad541c287
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-29 09:56:32 -0300

    alterando titulo e paragrafo

---
+++ js-curso-2-aula_-/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo de número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Escolha um número aleatório de 1 a 10:';
