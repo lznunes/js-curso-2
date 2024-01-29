391b928969c50ef04d5ecc971314a0cee5324ad1
Luiz Antonio Nunes Neves <lznunes@gmail.com>
2024-01-29 09:58:36 -0300

    click do botão

--- js-curso-2-aula_-/app.js
+++ js-curso-2-aula_-/app.js
@@ -3,3 +3,7 @@ titulo.innerHTML = 'Jogo de número secreto';
 
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número aleatório de 1 a 10:';
+
+function verificarChute() {
+    console.log('0 botão foi clicado')
+}
